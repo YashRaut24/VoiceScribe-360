7b62b4fd7e25b88afa99e029420964d17683538c
YashRaut24 <yashcodezzzz24@gmail.com>
2026-08-18 20:53:39 +0530

    Implement consultation socket tracking

--- Backend/socket/consultation.socket.js
+++ Backend/socket/consultation.socket.js
@@ -5,38 +5,54 @@ function registerConsultationSocket(io, socket) {
 
     socket.on('join-room', ({ roomId, role }) => {
 
-        socket.join(roomId);
+            socket.join(roomId);
 
-        // Save information on this socket
-        socket.roomId = roomId;
-        socket.role = role;
+            // Save information on this socket
+            socket.roomId = roomId;
+            socket.role = role;
 
-        // Create room if it doesn't exist
-        if (!rooms.has(roomId)) {
-            rooms.set(roomId, {
-                doctorConnected: false,
-                patientConnected: false
-            });
+            // Create room if it doesn't exist
+            if (!rooms.has(roomId)) {
+                rooms.set(roomId, {
+                    doctorConnected: false,
+                    patientConnected: false
+                });
+            }
+
+            const room = rooms.get(roomId);
+
+            // Update room state
+            if (role === 'doctor') {
+                room.doctorConnected = true;
+            }
+
+            if (role === 'patient') {
+                room.patientConnected = true;
+            }
+
+            console.log(roomId, room);
+
+            console.log("Broadcasting:", room);
+            io.to(roomId).emit('participant-update', room);
+
+    });
+    socket.on(
+        'transcript-update',
+        ({ roomId, transcript }) => {
+
+            if (!roomId || !transcript) {
+                return;
+            }
+
+            io.to(roomId).emit(
+                'transcript-update',
+                {
+                    transcript
+                }
+            );
+
         }
-
-        const room = rooms.get(roomId);
-
-        // Update room state
-        if (role === 'doctor') {
-            room.doctorConnected = true;
-        }
-
-        if (role === 'patient') {
-            room.patientConnected = true;
-        }
-
-        console.log(roomId, room);
-
-        console.log("Broadcasting:", room);
-        io.to(roomId).emit('participant-update', room);
-
-});
-
+    );
     socket.on('end-consultation', (roomId) => {
 
         io.to(roomId).emit('consultation-ended');
